5e94132900851585f8e9b490dbac18d377264a41
Luke Sturm <lukesturm96@gmail.com>
2017-03-01 15:06:36 -0800

    removed athlete.py and sturm.py from repository

--- athlete.py
+++
@@ -1,8 +0,0 @@
-class Athlete:
-	def __init__(self, name):
-		self.name = name
-		self.current_scores = [0, 0, 0, 0, 0, 0, 0]
-		self.current_start_values = [0, 0, 0, 0, 0, 0, 0]
-
-	def get_name(self):
-		return self.name
